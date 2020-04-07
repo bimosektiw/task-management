50186b4b9cba2cfc2e8b6fa8c27b520cb0db1dc7
Rahman Fadhil <rhmnfadhil@gmail.com>
2020-04-07 14:24:47 +0700

    feat: add foldering

---
+++ Task Management/Tasks/Controller/TasksViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TasksViewController.swift
+//  Task Management
+//
+//  Created by Rahman Fadhil on 07/04/20.
+//  Copyright © 2020 Bimo Sekti Wicaksono. All rights reserved.
+//
+
+import UIKit
+
+class TasksViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
