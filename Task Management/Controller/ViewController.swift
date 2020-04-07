f19d9d3e9e1f4ccd939793b052d7129404b8e0bd
Bimo Sekti Wicaksono <bimo.wicaksono@gramedia.digital>
2020-04-07 09:17:16 +0700

    manage directory

---
+++ Task Management/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Task Management
+//
+//  Created by Bimo Sekti Wicaksono on 04/04/20.
+//  Copyright © 2020 Bimo Sekti Wicaksono. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
