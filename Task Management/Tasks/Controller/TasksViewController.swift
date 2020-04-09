f6976d39f376fe4d1901d8955790c7566da4ed14
Rahman Fadhil <rhmnfadhil@gmail.com>
2020-04-09 08:04:05 +0700

    feat: add UI elements to tasks and new task page

--- Task Management/Tasks/Controller/TasksViewController.swift
+++ Task Management/Tasks/Controller/TasksViewController.swift
@@ -8,12 +8,47 @@
 
 import UIKit
 
-class TasksViewController: UIViewController {
+class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tasksTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        tasksTableView.delegate = self
+        tasksTableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return TaskList.shared.getTasksForCategoryNumber(section).count
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "!!!!"
+        case 1:
+            return "!!!"
+        case 2:
+            return "!!"
+        case 3:
+            return "!"
+        default:
+            return nil
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "taskCell", for: indexPath)
+        let task = TaskList.shared.getTasksForCategoryNumber(indexPath.section)[indexPath.row]
+        cell.textLabel?.text = task.title
+        return cell
     }
     
 
