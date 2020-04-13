ed0aaec695e4a98e7eef24420deeb5b73511453b
Bimo Sekti Wicaksono <bimo.wicaksono@gramedia.digital>
2020-04-13 11:47:43 +0700

    update layout task list

--- Task Management/Tasks/Controller/TasksViewController.swift
+++ Task Management/Tasks/Controller/TasksViewController.swift
@@ -15,6 +15,8 @@ protocol TasksDelegate {
 class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, TasksDelegate {
 
     @IBOutlet weak var tasksTableView: UITableView!
+    @IBOutlet weak var roundedView: UIView!
+    @IBOutlet weak var coinLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,8 +25,30 @@ class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDat
         
         tasksTableView.delegate = self
         tasksTableView.dataSource = self
+        setupView()
+    }
+    
+    func setupView() {
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(onEditTapped))
+        let label = UILabel(frame: CGRect(x: 10, y: 0, width: 50, height: 40))
+        label.font = UIFont.boldSystemFont(ofSize: 17)
+
+        label.text = "My Tasks"
+        label.numberOfLines = 2
+        label.textColor = .black
+        label.sizeToFit()
+        label.textAlignment = .center
+
+        self.navigationItem.titleView = label
+        navigationItem.largeTitleDisplayMode = .never
+        
+        roundedView.layer.borderColor = #colorLiteral(red: 0.5921568627, green: 0.5921568627, blue: 0.5921568627, alpha: 1)
+        roundedView.layer.borderWidth = 1
+        roundedView.layer.cornerRadius = 10
+        
+        tasksTableView.tableFooterView = UIView(frame: .zero)
+        
     }
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
@@ -35,7 +59,19 @@ class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDat
         }
     }
     
+    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int){
+        view.tintColor = #colorLiteral(red: 0.9466527104, green: 0.9426648021, blue: 0.9497463107, alpha: 1)
+        let header = view as! UITableViewHeaderFooterView
+        header.textLabel?.textColor = UIColor.black
+    }
+    
     @objc func onEditTapped() {
+        if tasksTableView.isEditing {
+            navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(onEditTapped))
+            
+        } else {
+            navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(onEditTapped))
+        }
         tasksTableView.isEditing = !tasksTableView.isEditing
     }
     
@@ -47,6 +83,7 @@ class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDat
         let task = TaskList.shared.getTasksForCategoryNumber(sourceIndexPath.section)[sourceIndexPath.row]
         TaskList.shared.deleteTaskById(id: task.id)
         TaskList.shared.insert(title: task.title, duration: task.duration, category: destinationIndexPath.section)
+        tasksTableView.reloadData()
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -54,7 +91,11 @@ class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDat
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 4
+        if TaskList.shared.getAllTask().count == 0 {
+            return 0
+        } else {
+            return 4
+        }
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
@@ -76,8 +117,27 @@ class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDat
         let cell = tableView.dequeueReusableCell(withIdentifier: "taskCell", for: indexPath)
         let task = TaskList.shared.getTasksForCategoryNumber(indexPath.section)[indexPath.row]
         cell.textLabel?.text = task.title
+        cell.textLabel?.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
         if task.completed {
             cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
+        switch task.category {
+        case 0:
+            cell.backgroundColor = #colorLiteral(red: 0.9176470588, green: 0.09019607843, blue: 0.2666666667, alpha: 1)
+            break
+        case 1:
+            cell.backgroundColor = #colorLiteral(red: 0, green: 0.6745098039, blue: 0.7803921569, alpha: 1)
+            break
+        case 2:
+            cell.backgroundColor = #colorLiteral(red: 0.337254902, green: 0.7803921569, blue: 0, alpha: 1)
+            break
+        case 3:
+            cell.backgroundColor = #colorLiteral(red: 0.7058823529, green: 0.7058823529, blue: 0.7058823529, alpha: 1)
+            break
+        default:
+            cell.backgroundColor = .clear
         }
         return cell
     }
@@ -95,6 +155,7 @@ class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDat
             }))
             present(alert, animated: true, completion: nil)
         }
+        tasksTableView.deselectRow(at: indexPath, animated: true)
     }
     
 
@@ -110,5 +171,13 @@ class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDat
     func insertTask(title: String, category: Int, duration: Int) {
         TaskList.shared.insert(title: title, duration: duration, category: category)
         tasksTableView.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        if tasksTableView.isEditing {
+            return true
+        } else {
+            return false
+        }
     }
 }
