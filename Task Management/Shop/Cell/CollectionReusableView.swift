8b899cca3444a3a82929d9d453e98ef276b6b933
Alldo Kurniawan <alldo_k@yahoo.com>
2020-05-04 12:18:20 +0700

    Keluar List, belum bisa Buy

---
+++ Task Management/Shop/Cell/CollectionReusableView.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionReusableView.swift
+//  Task Management
+//
+//  Created by Alldo Kurniawan on 13/04/20.
+//  Copyright © 2020 Bimo Sekti Wicaksono. All rights reserved.
+//
+
+import UIKit
+
+class CollectionReusableView: UICollectionReusableView {
+        
+}
